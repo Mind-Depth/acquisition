590beeafd2f7f99c321c1e0269716a70f4509161
Benjamin Roux <benjamin2.roux@epitech.eu>
2019-01-26 18:37:05 +0000

    Add basics tests on ia training dataset

---
+++ DemoAcqui/Tests/TrainingDataConsistencyTest.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python3
+
+import unittest
+import numpy
+from numpy import loadtxt
+
+class TestStringMethods(unittest.TestCase):
+
+    def load_csv(self):
+        dataset = loadtxt('testfile.csv', delimiter=",")
+        return dataset
+        
+    def test_minimum_row_amount_relevance(self):
+        dataFile = self.load_csv()
+        self.assertGreaterEqual(len(dataFile), 500)
+
+    def test_global_content(self):
+        dataFile = self.load_csv()
+        for dataBuf in dataFile:
+            self.assertEqual(dataBuf.size, 11)
+
+    def test_data_consistency(self):
+        dataFile = self.load_csv()
+        for dataBuf in dataFile:
+            i = 0
+            for data in dataBuf:
+                if (i >= 0 and i <= 9):
+                    self.assertGreaterEqual(len(str(int(data))), 2)
+                else:
+                    self.assertEqual(len(str(int(data))), 1)
+                i += 1
+            
+
+if __name__ == '__main__':
+    unittest.main()
