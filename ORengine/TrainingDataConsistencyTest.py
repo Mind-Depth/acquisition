34d2496e5cb98e3b80130ff145b9c36c11dfe7b5
Benjamin Roux <benjamin2.roux@epitech.eu>
2019-11-08 14:42:19 +0100

    first try  fix ut

--- ORengine/TrainingDataConsistencyTest.py
+++ ORengine/TrainingDataConsistencyTest.py
@@ -7,7 +7,7 @@ from numpy import loadtxt
 class TestTrainingDataConsistency(unittest.TestCase):
 
     def load_csv(self):
-        dataset = loadtxt('../TrainingDataset/testfile.csv', delimiter=",")
+        dataset = loadtxt('../TrainingDataset/defautlDataset.csv', delimiter=",")
         return dataset
         
     def test_minimum_row_amount_relevance(self):
